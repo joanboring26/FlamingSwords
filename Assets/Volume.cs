5c7b74bd8a66fde1816959c42616185ea51a88c8
Cuesta <david.cuesta@enti.cat>
2020-06-02 13:54:40 +0200

    Intro particles minor fix

---
+++ Assets/Volume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    
+    public Button increase;
+    public Button decrease;
+    public Button[] volButtons;
+
+
+    void Start()
+    {
+        for()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
